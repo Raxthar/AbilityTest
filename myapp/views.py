fd9b1f29b08797e4ca1d5898ecdc1df5e94eaaeb
huangguisheng <1135409475@qq.com>
2019-01-05 18:15:43 +0800

    Preliminary preparation for adding dimension API. Ref #14

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 from django.views.decorators.http import require_http_methods
@@ -6,39 +7,23 @@ from django.http import JsonResponse
 import json
 
 # Create your views here.
-from .models import Book
+from .models import Dimension
 
 
-@require_http_methods(["GET"])
-def add_book(request):
-    response = {}
+def createDimension(request):
+    print('create')
+    obj = json.loads(request.body)
+    dName = obj['dName']
+    tID = obj['tID']
     try:
-        book = Book(book_name=request.GET.get('book_name'))
-        book.save()  # 存入数据库
-        response['msg'] = 'success'
-        response['error_num'] = 0
+        dimension = Dimension(dName=dName, tID=tID)  # 填数据库时还要测评号tID，怎么拿到测评号tID呢？在这里先假设拿到了.
+        dimension.save()
+        result = {
+            "code": 200,
+        }
     except Exception as e:
-        response['msg'] = str(e)
-        response['error_num'] = 1
-
-    return JsonResponse(response)  # 使用JSON格式来包装数据
-
-
-@require_http_methods(['GET'])
-def show_books(request):
-    response = {}
-    try:
-        books = Book.objects.filter()  # 从数据库中查询会得到符合条件的model对象的列表？
-        response['list'] = json.loads(serializers.serialize("json", books))  # 这里将得到的books列表序列化变成json格式对象
-        # json.load()是将json格式数据转换为字典
-        response['msg'] = 'success'
-        response['error_num'] = 0
-    except Exception as e:
-        response['msg'] = str(e)
-        response['error_num'] = 1
-
-    return JsonResponse(response)
-
-
-
-
+        result = {
+            "code": 0,
+            "errMsg": e
+        }
+    return HttpResponse(json.dumps(result), content_type="application/json")
