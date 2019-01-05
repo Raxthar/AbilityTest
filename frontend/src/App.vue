e0dc4b47b2db40b129cfe9bc2918e140ef2d1b7e
zhengsiyuan <178587528@qq.com>
2019-01-05 17:18:20 +0800

    Modified App.vue to enable jumps. Ref #16

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div>
+    <Breadcrumb>
+      <BreadcrumbItem to="/HelloWorld">Home</BreadcrumbItem>
+      <BreadcrumbItem to="/addtitle">AddEvaluationTitle</BreadcrumbItem>
+      <BreadcrumbItem to="/adddims">AddDimensions</BreadcrumbItem>
+    </Breadcrumb>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'app',
+  methods: {
+    gotest () {
+      this.$router.push('/')
+    }
+  }
 }
 </script>
 
