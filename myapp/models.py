6a5ee87e2b1aaf1bdcdb34325937d47d52c9c307
何雨玲 <1152598331@qq.com>
2019-01-05 19:00:51 +0800

    Test title and description interface.Ref #9

--- myapp/models.py
+++ myapp/models.py
@@ -4,25 +4,25 @@ from django.db import models
 
 
 class ATest(models.Model):
-    tID = models.IntegerField(primary_key=True, db_column='FId')
+    tID = models.IntegerField(primary_key=True, db_column='tId')
     tName = models.CharField(max_length=30)
     tDescribe = models.CharField(max_length=80)
 
 
 class Question(models.Model):
-    qID = models.IntegerField(primary_key=True, db_column='FId')
+    qID = models.IntegerField(primary_key=True, db_column='qID')
     qName = models.CharField(max_length=30)
     tID = models.ForeignKey(ATest, on_delete=models.CASCADE)
 
 
 class Dimension(models.Model):
-    dID = models.IntegerField(primary_key=True, db_column='FId')
+    dID = models.IntegerField(primary_key=True, db_column='dID')
     dName = models.CharField(max_length=30)
     tID = models.ForeignKey(ATest, on_delete=models.CASCADE)
 
 
 class Option(models.Model):
-    oID = models.IntegerField(primary_key=True, db_column='FId')
+    oID = models.IntegerField(primary_key=True, db_column='oID')
     oName = models.CharField(max_length=30)
     qID = models.ForeignKey(Question, on_delete=models.CASCADE)
     score = models.IntegerField()
--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 # 告诉django对于某段url应该调用哪段代码
 # url ( 正则表达式，view函数，传递给视图的参数， 别名（命名空间），前缀 ),
-urlpatterns = [url(r'add_book$', views.add_book, ),
-               url(r'show_books$', views.show_books, ), ]  # 正则表达式表示url
+urlpatterns = [url('create/', views.create, name='create'),]  # 正则表达式表示url
