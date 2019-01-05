c4f84041793fc1428e8e0fca48bad912bf472a77
zhengsiyuan <178587528@qq.com>
2019-01-05 17:26:05 +0800

    Modified index.js setting routing. Ref #15

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import addtitle from '@/components/AddEvaluationTitle'
+import adddims from '@/components/AddDimensions'
 
 Vue.use(Router)
 
 export default new Router({
+  model: 'hash',
   routes: [
     {
-      path: '/',
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/addtitle',
+      name: 'addtitle',
+      component: addtitle
+    },
+    {
+      path: '/adddims',
+      name: 'adddims',
+      component: adddims
     }
   ]
 })
