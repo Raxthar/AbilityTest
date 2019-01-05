20e38c465e83ac49dacf10ed6fc8983e32761d12
huangguisheng <1135409475@qq.com>
2019-01-05 16:48:53 +0800

    Add new models.py to master. Ref #1

--- myapp/models.py
+++ myapp/models.py
@@ -3,9 +3,27 @@ from django.db import models
 # Create your models here.
 
 
-class Book(models.Model):
-    book_name = models.CharField(max_length=64)
-    add_time = models.DateTimeField(auto_now_add=True)
+class ATest(models.Model):
+    tID = models.IntegerField(primary_key=True, db_column='FId')
+    tName = models.CharField(max_length=30)
+    tDescribe = models.CharField(max_length=80)
 
-    def _unicode_(self):
-        return self.book_name
+
+class Question(models.Model):
+    qID = models.IntegerField(primary_key=True, db_column='FId')
+    qName = models.CharField(max_length=30)
+    tID = models.ForeignKey(ATest, on_delete=models.CASCADE)
+
+
+class Dimension(models.Model):
+    dID = models.IntegerField(primary_key=True, db_column='FId')
+    dName = models.CharField(max_length=30)
+    tID = models.ForeignKey(ATest, on_delete=models.CASCADE)
+
+
+class Option(models.Model):
+    oID = models.IntegerField(primary_key=True, db_column='FId')
+    oName = models.CharField(max_length=30)
+    qID = models.ForeignKey(Question, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    dID = models.ForeignKey(Dimension, on_delete=models.CASCADE)
