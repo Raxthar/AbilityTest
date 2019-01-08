23e50f498b6a5078fc6bc54a9e41c7a10e0a9c9c
何雨玲 <1152598331@qq.com>
2019-01-08 17:05:28 +0800

    Creat the test and description Ref #33

--- myapp/views.py
+++ myapp/views.py
@@ -69,3 +69,17 @@ def create(request):
             "errMsg": e
         }
     return HttpResponse(json.dumps(res), content_type="application/json")
+
+
+def create(request):
+    obj = json.loads(request.body)
+    test_name = obj['tName']
+    test_describe = obj['tDescribe']
+    _id = obj['uID']
+    test_id = {}
+    test = Test(tName=test_name, qDescribe=test_describe, status=0, id=_id, Due="")
+    test.save()
+    test1 = Test.objects.get(tName=test_name, qDescribe=test_describe, status=0, id=_id, Due="")
+    print(test1.tID)
+    test_id['data'] = test1.tID
+    return JsonResponse(test_id)
