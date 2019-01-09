6975a6e0dacb59fda7a0720fa9051140814e901b
suyunpeng <894624801@qq.com>
2019-01-09 17:28:23 +0800

    Add the port for the adding question page. Ref #58

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     url('create/', views.create, name='create'),
     url('create_dimension/', views.create_dimension, name='create_dimension'),
+    url('add_question/', views.add_question, name='add_question'),
     url('search_all_atest/', views.search_all_atest, name='search_all_atest'),
     url('create_judge/', views.create_judge, name='create_judge'),
     url('set_dimension_page/', views.set_dimension_page, name='set_dimension_page'),
