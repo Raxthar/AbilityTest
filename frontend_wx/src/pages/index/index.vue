0f8e9c782436daca1b74dd3eb2ff9ff805ee5bd7
suyunpeng <894624801@qq.com>
2019-01-13 18:25:30 +0800

    Update the index page to connect the server. Ref #101

--- frontend_wx/src/pages/index/index.vue
+++ frontend_wx/src/pages/index/index.vue
@@ -1,30 +1,82 @@
 <template>
   <div >
-    <i-button type="primary" size="small" @click="createbtn" >创建测评</i-button>
-    <i-button type="primary" size="small" @click="testtitle" >测试</i-button>
-    <i-card title="龙卷风摧毁停车场" extra="额外内容" >
-      <view slot="footer">测评编号</view>
+    <i-massage id="message" />
+    <i-button type="primary" size="small" @click="testtitle" >创建测评</i-button>
+    <i-card v-for="(evaluation, index) in evaluationLists" v-bind:key="index" :title="evaluation.tName" @click="handleOpen">
+      <view slot="footer">{{evaluation.tStatus}}</view>
     </i-card>
-    <i-card title="乌鸦坐飞机" extra="额外内容" >
-      <view slot="footer">测评编号</view>
-    </i-card>
-    <i-card title="poipoipoi" extra="额外内容" >
-      <view slot="footer">测评编号</view>
-    </i-card>
+    <i-action-sheet :visible="this.visible" :actions="this.action" show-cancel bind:cancel="handleCancel" @click="handleClickItem"
+    :mask-closable="false" />
   </div>
 </template>
 
 <script>
 
 export default {
+  mounted () {
+    this.searchEvaluation()
+  },
+
   data () {
     return {
+      visible: false,
+      actions: [
+        {
+          name: '查看详细信息'
+        },
+        {
+          name: '删除测评',
+          color: '#ed3f14'
+        },
+        {
+          name: '修改测评'
+        },
+        {
+          name: '去分享',
+          icon: 'share',
+          openType: 'share'
+        }
+      ],
       msg: 'Hello',
-      u_id: 2
+      uId: 2,
+      evaluationLists: []
     }
   },
 
   methods: {
+    searchEvaluation () {
+      let lists = this.evaluationLists
+      wx.request({
+        url: 'http://127.0.0.1:8000/test_list/',
+        data: {
+          uId: this.uId
+          },
+        success (response) {
+          console.log(response.data)
+          if(response.data.code === 200) {
+            if(response.data.tName.length > 0) {
+              for(let i = 0; i < response.data.tName.length; i++) {
+                let obj = {
+                  tName: response.data.tName[i],
+                  tStatus: response.data.tStatus[i] ? '已发布':'未发布'
+                }
+                lists.push(obj)
+              }
+            }
+          } else {
+            $Message({
+              content: '读取信息错误！',
+              type: 'error'
+            })
+          }
+        }
+      })
+      this.evaluationLists = lists
+    },
+
+    handleOpen() {
+
+    },
     createbtn () {
       wx.navigateTo({
         url: '../create_demision/main'
