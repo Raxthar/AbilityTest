ecc71ad3d46119d69b29acb6c8ffb2a68cf79481
zhengsiyuan <178587528@qq.com>
2019-01-13 09:56:44 +0800

    Added a new page showing statistics. Ref #99

---
+++ frontend/src/components/EvaluationStat.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="layout">
+    <Layout>
+      <Header>
+        <Button :size="buttonSize" type="primary" to="/HelloWorld">
+          <Icon type="ios-arrow-back" />
+          返回
+        </Button>
+      </Header>
+      <Content>
+        <Button :size="buttonSize" type="primary" @click="jumpToAddTitle">
+          创建测评
+        </Button>
+      </Content>
+      <Footer>
+        <card>
+        </card>
+      </Footer>
+    </Layout>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    this.searchStat()
+  },
+  data () {
+    return {
+      tId: this.$route.params.tId
+    }
+  },
+  methods: {
+      searchStat () {
+        this.$axios.get('test_stat', {
+        params: {
+          tId: this.tId
+        }
+      }).then(message => {
+        if (message.data.code === 200) {
+          if (message.data.tName.length > 0) {
+            
+          }
+        } else {
+          this.$Message.error(`can't search in database`)
+        }
+      })
+      }
+  }
+}
+</script>
+
+
+<style>
+.layout {
+  height: 100vmin;
+}
+
+.ivu-layout-header {
+  background-color: rgb(97, 176, 255);
+  padding: 0 20px;
+}
+
+.ivu-layout-content {
+  height: 80px;
+  padding: 20px;
+}
+
+.ivu-btn-primary {
+  background-color: rgb(97, 176, 255);
+  border-color: rgb(97, 176, 255);
+}
+
+.ivu-btn-large {
+  font-size: 18px;
+}
+
+.ivu-card {
+  height: 100vmin;
+  margin: auto auto;
+  text-align: center;
+}
+
+.ivu-card-body {
+  height: 500px;
+  padding: 100px;
+}
+
+.ivu-card-bordered {
+  border: 1px solid #f9f9fa;
+  border-color: #fafafa;
+}
+</style>
