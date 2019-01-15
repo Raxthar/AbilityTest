035997de9b1f18af5b3f670a22743678f3287992
何雨玲 <1152598331@qq.com>
2019-01-15 19:55:20 +0800

    Realize the function of setting deadline and automatic evaluation in front of small program. Ref #103

---
+++ frontend_wx/src/pages/set_deadline/index.vue
@@ -0,0 +1,120 @@
+<template>
+   <div>
+      <Form :model="dimensions">
+            <FormItem v-for="(Item,index) in judges.dimensionName" v-bind:key="index">
+              <p>{{Item}}</p>
+              <Input :value="value" @click="handleClick(index)" @input="handleInput" size="large" placeholder="请输入评价" class="demo-input"/>
+            </FormItem>
+            <p>截止日期</p>
+            <input v-model="judges.due" type="textarea"  placeholder="请输入截止日期" maxlength="50" class="demo-input"/>
+               <i-button type="primary" @click="editJudge">提交</i-button>
+      </Form>
+ </div>
+</template>
+
+<script>
+  import {$Message} from '../../../static/iview/base/index'
+  export default {
+    mounted (options) {
+      this.searchDimension()
+      // this.tId=this.$root.$mp.query.tId
+    // this.judges.tId=this.$root.$mp.query.tId
+    },
+    data () {
+      return {
+        currentIndex: -1,
+        tId: 3,
+        value: "",
+        judges: {
+          dimensionId: [],
+          dimensionName: [],
+          judge: [],
+          due: '',
+          tId: 3
+        }
+      }
+    },
+    methods: {
+      handleClick (index) {
+        this.currentIndex = index
+      },
+      handleInput (e) {
+        this.judges.judge[this.currentIndex] = e.target.value
+      },
+      searchDimension () {
+        let list = this.judges
+        wx.request({
+          url: 'http://127.0.0.1:8000/search_dimensions/', // 仅为示例，并非真实的接口地址
+          method: "GET",
+          header: {
+            "content-type": "application/x-www-form-urlencoded"
+          },
+          data: {content: this.judges.tId},
+          success (response) {
+            console.log(response.data)
+            if (response.data.code === 200) {
+              if (response.data.dId.length > 0) {
+                for (let i = 0; i < response.data.dId.length; i++) {
+                  list.dimensionName = response.data.dName
+                  list.dimensionId = response.data.dId
+                  console.log(list.dimensionName)
+                }
+              }
+            } else {
+              $Message({
+                content: `无法读取数据库`,
+                type: 'error'
+              })
+            }
+          }
+        })
+        this.judges = list
+      },
+      editJudge () {
+        wx.request({
+          url: 'http://127.0.0.1:8000/edit_judge/', // 仅为示例，并非真实的接口地址
+          method: "POST",
+          header: {
+            "content-type": "application/x-www-form-urlencoded"
+          },
+          data: JSON.stringify(this.judges),
+          success (response) {
+            console.log(response.data)
+            if (response.data.code === 200) {
+              wx.navigateTo({
+                url: '../question_list/main'
+              })
+              $Message({
+                content: `设置评价成功`,
+                type: 'success'
+              })
+            } else {
+              $Message({
+                content: `无法读取数据库`,
+                type: 'error'
+              })
+            }
+          }
+        })
+      }
+    }
+  }
+</script>
+
+
+<style scoped> 
+.demo-input{
+    color:black;
+    padding: 7px 15px;
+    border-radius: 4px;
+    min-width: 65px;
+    flex: 1;
+    line-height: 1.6;
+    padding: 4px 0;
+    min-height: 22px;
+    font-size: 14px;
+    border: 1px solid;
+    border-color: rgb(103, 103, 241);
+    width:auto;
+ }
+</style>
