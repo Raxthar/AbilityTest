3c0c5ed50859f123ea5096ad373aec1bd8c08023
何雨玲 <1152598331@qq.com>
2019-01-15 19:38:13 +0800

    Improve the front-end of small programs to add topic pages. Ref #129

---
+++ frontend_wx/src/pages/create_question/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './index'
+const app = new Vue(App)
+app.$mount()
+export default {
+  'usingComponents': {
+
+    'i-button': '/node_modules/iview-weapp/dist/button/index',
+    'i-modal': '../../../node_modules/iview-weapp/dist/modal/index',
+    'i-row': '../../../node_modules/iview-weapp/dist/row/index',
+    'i-col': '../../../node_modules/iview-weapp/dist/col/index',
+    'i-panel': '../../../node_modules/iview-weapp/dist/panel/index'
+  }
+}
