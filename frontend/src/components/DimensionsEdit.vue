69d3a4debb105cc30fb72d3b11ba6e0468a9527b
zhengsiyuan <178587528@qq.com>
2019-01-17 20:41:07 +0800

    Adding unit tests for dimension editing page. Ref #151

--- frontend/src/components/DimensionsEdit.vue
+++ frontend/src/components/DimensionsEdit.vue
@@ -9,8 +9,8 @@
       </Header>
       <Content>
         <Card>
-          <Button :size="buttonSize" type="primary" @click="addDimension">添加维度</Button>
-          <Button :size="buttonSize" type="primary" @click="delDimension">删除维度</Button><br><br>
+          <Button :size="buttonSize" type="primary" @click="addDimension" id="addButton">添加维度</Button>
+          <Button :size="buttonSize" type="primary" @click="delDimension" id="delButton">删除维度</Button><br><br>
           <Form :model="dimensions">
             <FormItem v-for="(list, index) in dimensions.dimensionName" :key="(list, index)">
               <Input v-model="dimensions.dimensionName[index]" style="width: 600px" size="large" placeholder="请输入维度" />
@@ -47,10 +47,11 @@ export default {
       let cope = {
         index: ''
       }
-      this.lists.push(cope)
+      this.dimensions.dimensionName.push(cope)
     },
-    delDimension: function (index) {
-      this.lists.splice(index, 1)
+    delDimension: function () {
+      let count = this.dimensions.dimensionName.length - 1
+      this.dimensions.dimensionName.splice(count, 1)
     },
     searchDimensions () {
       this.$axios.get('search_dimensions', {
