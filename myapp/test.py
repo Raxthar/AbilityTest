9a53535317f05196cea2578afaa29e0de4c018c7
huangguisheng <1135409475@qq.com>
2019-01-19 17:11:35 +0800

    Add unittest of test_question_chat. Ref #200

--- myapp/test.py
+++ myapp/test.py
@@ -131,3 +131,19 @@ class ViewTest(unittest.TestCase):
         json.dumps({"tId": 1, "qName": "asd", "oName": ["sdsd"], "score": [5], "dId": [5]}),
         content_type="application/json")
         self.failUnlessEqual(response.status_code, 200)
+
+
+    def test_add_question(self):
+        self.client = Client()
+        response = self.client.post("/add_question/",
+        json.dumps({"tId": 1, "qName": "asd", "oName": ["sdsd"], "score": [5], "dId": [5]}),
+        content_type="application/json")
+        self.failUnlessEqual(response.status_code, 200)
+
+
+    def test_question_chat(self):
+        self.client = Client()
+        response = self.client.post("/question_chat/",
+            json.dumps({"tId": 1, "qName": "asd", "oName": ["sdsd"], "score": [5], "dId": [5]}), 
+            content_type="application/json")
+        self.failUnlessEqual(response.status_code, 200)
